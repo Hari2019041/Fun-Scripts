0427147b2e65bfad8b9dc8ecb2ca7f53f291db1e
Hari2019041 <hari19041@iiitd.ac.in>
2020-12-01 14:36:37 +0530

    Formatting to PEP8

--- collatz_conjecture.py
+++ collatz_conjecture.py
@@ -4,32 +4,32 @@ list_of_steps = []
 paths = [""]*N
 paths[0] = "1"
 
+
 def finding_path(i):
-	steps = 0
-	no = i
+    steps = 0
+    no = i
 
-	while no != 1:
-		if no < i:
-			paths[i-1] += paths[int(no)-1]
-			steps += list_of_steps[int(no)-1]	
-			break
+    while no != 1:
+        if no < i:
+            paths[i-1] += paths[int(no)-1]
+            steps += list_of_steps[int(no)-1]
+            break
 
-		paths[i-1] += str(int(no)) + "->"
+        paths[i-1] += str(int(no)) + "->"
 
-		if no%2 == 0:
-			no = no/2
-			steps += 1
-		else:
-			no = (3*no+1)/2
-			steps += 2
+        if no % 2 == 0:
+            no = no/2
+            steps += 1
+        else:
+            no = (3*no+1)/2
+            steps += 2
 
-	if paths[i-1][-1] != "1":
-		paths[i-1] += "1"
+    if paths[i-1][-1] != "1":
+        paths[i-1] += "1"
 
-	list_of_steps.append(steps)
+    list_of_steps.append(steps)
+
 
 for i in range(1, N+1):
-	finding_path(i)
-	print(paths[i-1])
-
-
+    finding_path(i)
+    print(paths[i-1])
--- the_19_challenge.py
+++ the_19_challenge.py
@@ -1,33 +1,34 @@
 def is_prime(p):
-	for i in range(2, p):
-		if p%i == 0:
-			return False
+    for i in range(2, p):
+        if p % i == 0:
+            return False
 
-	return True
+    return True
+
 
 def find_primes(N):
-	primes = []
-	i = 2
-	while (len(primes) < N):
-		if is_prime(i):
-			primes.append(i)
-		i += 1
+    primes = []
+    i = 2
+    while (len(primes) < N):
+        if is_prime(i):
+            primes.append(i)
+        i += 1
 
-	return primes
+    return primes
+
 
 def check_property(primes):
-	total = 0
-	for prime in primes:
-		total += prime**2
+    total = 0
+    for prime in primes:
+        total += prime**2
 
-	return int(total)%len(primes) == 0
+    return int(total) % len(primes) == 0
+
 
 candidates = []
 
 for i in range(1, 101):
-	if check_property(find_primes(i)):
-		candidates.append(i)
+    if check_property(find_primes(i)):
+        candidates.append(i)
 
 print(candidates)
-
-
