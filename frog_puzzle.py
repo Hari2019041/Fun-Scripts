3b69063473a39ca050a6843ef96716db8456aede
Hari <hari19041@iiitd.ac.in>
2021-03-05 15:59:09 +0530

    Added the rules of the puzzle and did some clean coding

--- frog_puzzle.py
+++ frog_puzzle.py
@@ -1,31 +1,33 @@
-# There exists a pond with certain no of lily pads in it and a frog before it
-# and it wishes to get to the last lily pad.
-# It can only move forward. At any given pad, it can hop to the other pads with
-# equal probability.
-# This is a simulation to find out the average number of hops the frog will
-# take to reach the last lily pad.
+"""
+    There exists a pond with certain no of lily pads in it and a frog before it
+    and it wishes to get to the last lily pad.
+    It can only move forward. At any given pad, it can hop to any of the
+    forward pads with equal probability.
+    This is a simulation to find out the average number of hops the frog will
+    take to reach the last lily pad.
+"""
 
 from random import randint
 
-no_of_simulations = 10**6
-size_of_pond = 100
+NO_OF_SIMULATIONS = 10**6
+SIZE_OF_POND = 10
 no_of_steps_list = []
 
 
-def simulation(size_of_pond):
+def simulation(SIZE_OF_POND):
     steps = 0
     pos = 0
 
-    while (pos != size_of_pond):
-        jump = randint(1, size_of_pond-pos)
+    while (pos != SIZE_OF_POND):
+        jump = randint(1, SIZE_OF_POND-pos)
         pos += jump
         steps += 1
 
     no_of_steps_list.append(steps)
 
 
-for i in range(no_of_simulations):
-    simulation(size_of_pond)
+for i in range(NO_OF_SIMULATIONS):
+    simulation(SIZE_OF_POND)
 
-average = sum(no_of_steps_list)/no_of_simulations
+average = sum(no_of_steps_list)/NO_OF_SIMULATIONS
 print(average)
