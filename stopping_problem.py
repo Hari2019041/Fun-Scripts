adb61ae699a165526bc3ae56a76960510bb779c7
Hari <hari19041@iiitd.ac.in>
2021-03-05 12:56:30 +0530

    Added stopping problem/mathematical way to find a toilet

---
+++ stopping_problem.py
@@ -0,0 +1,28 @@
+from random import randint
+
+NO_OF_TEST_CASES = 1000
+NO_OF_TOILETS = 1000
+QUALITY_RANGE = 10**10
+
+STOPPING_POINT = int(0.37*NO_OF_TOILETS)
+successes = 0
+
+for test_case in range(NO_OF_TEST_CASES):
+    toilets = [randint(0, QUALITY_RANGE) for i in range(NO_OF_TOILETS)]
+    best_toilet = max(toilets)
+    best_toilet_yet = -1
+
+    for i in range(NO_OF_TOILETS):
+        if i <= STOPPING_POINT:
+            if toilets[i] >= best_toilet_yet:
+                best_toilet_yet = toilets[i]
+        else:
+            if toilets[i] > best_toilet_yet:
+                best_toilet_yet = toilets[i]
+                if best_toilet_yet == best_toilet:
+                    successes += 1
+                break
+
+probability = successes/NO_OF_TEST_CASES
+
+print(probability)
