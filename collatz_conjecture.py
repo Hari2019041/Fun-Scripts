ac6cc7adbef1b986008e8b55d639d55c19d0d77e
Hari <hari19041@iiitd.ac.in>
2021-03-05 15:56:42 +0530

    Added documentation and theory

--- collatz_conjecture.py
+++ collatz_conjecture.py
@@ -1,11 +1,21 @@
-N = 10**4
+"""
+    This is a fun testing of the famous unsolved Collatz Conjecture.
+    The rules are as follows:
+        If n is odd: n -> 3n+1
+        If n is even: n -> n/2
+    The Collatz Conjecture states that every number will eventually go to 1
+    after a certain number of steps.
+    This is to test and find the steps for the numbers.
+"""
+
+N = 10**5
 
 list_of_steps = []
-paths = [""]*N
-paths[0] = "1"
+paths = ['']*N
+paths[0] = '1'
 
 
-def finding_path(i):
+def find_path(i):
     steps = 0
     no = i
 
@@ -15,7 +25,7 @@ def finding_path(i):
             steps += list_of_steps[int(no)-1]
             break
 
-        paths[i-1] += str(int(no)) + "->"
+        paths[i-1] += str(int(no)) + '->'
 
         if no % 2 == 0:
             no = no/2
@@ -24,12 +34,12 @@ def finding_path(i):
             no = (3*no+1)/2
             steps += 2
 
-    if paths[i-1][-1] != "1":
-        paths[i-1] += "1"
+    if paths[i-1][-1] != '1':
+        paths[i-1] += '1'
 
     list_of_steps.append(steps)
 
 
 for i in range(1, N+1):
-    finding_path(i)
+    find_path(i)
     print(paths[i-1])
