d89235e2a32fdf289226563d552f45e2c387518a
Hari2019041 <hari19041@iiitd.ac.in>
2021-05-16 00:23:47 +0530

    Changed distance function, cleaned code by using color variable

--- Pi-Calculation/darts.py
+++ Pi-Calculation/darts.py
@@ -26,14 +26,14 @@ def setWindow(TITLE):
 
 
 def distance(point, center=CENTER):
-    return (point[0] - center[0])**2 + (point[1] - center[1])**2
+    return ((point[0] - center[0])**2 + (point[1] - center[1])**2)**0.5
 
 
 def main():
     setWindow(TITLE)
 
     RUNNING = True
-    NO_OF_POINTS = 10000
+    NO_OF_POINTS = 100
     total_points = 0
     inside_circle = 0
 
@@ -43,21 +43,19 @@ def main():
 
         pygame.draw.circle(SCREEN, BLACK, CENTER, RADIUS, 1)
 
+        total_points += NO_OF_POINTS
         for i in range(NO_OF_POINTS):
             point = (randint(2, WIDTH - 1), randint(2, HEIGHT - 30 - 1))
-            if distance(point)**0.5 >= RADIUS:
-                pygame.draw.circle(SCREEN, RED, point, 1)
-            else:
-                inside_circle += 1
-                pygame.draw.circle(SCREEN, GREEN, point, 1)
-        total_points += NO_OF_POINTS
+            color = RED if distance(point) >= RADIUS else GREEN
+            inside_circle += 1 if distance(point) <= RADIUS else 0
+            pygame.draw.circle(SCREEN, color, point, 1)
 
         pygame.draw.rect(SCREEN, WHITE, (0, HEIGHT - 30, WIDTH, 30))
         pi_estimate = float(4 * inside_circle / total_points)
         pi_label = FONT.render(
             "PI: " + "{:.20f}".format(pi_estimate), True, BLACK, WHITE)
-        SCREEN.blit(pi_label, (0, 604))
+        SCREEN.blit(pi_label, (0, WIDTH))
         pygame.display.update()
 
-
-main()
+if __name__ == '__main__':
+    main()
